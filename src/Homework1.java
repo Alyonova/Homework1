dc1b5e9b83056f91787ef4fd274373ac6325018e
Alyonova <alenka.shum@gmail.com>
2023-10-15 22:37:54 -0700

    Initial commit

---
+++ src/Homework1.java
@@ -0,0 +1,14 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Значение a:" + a);
+        System.out.println("Значение b:" + b);
+
+    }
+}
